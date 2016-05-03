93298b303f45b64523f042e8b67b71599cacdd8b
saylerb <sayler.b@gmail.com>
2016-05-03 16:55:28 -0600

    fix random number

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,8 +1,25 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
+number = rand(100)
+
+# aasdfasdfasdsaf
+
 get '/' do
-  number = rand(100)
-  erb :index, :locals => {:number => number}
+  guess = params[:guess].to_i 
+
+  # require "pry"; binding.pry
+  if guess == number
+    msg = "You guessed the number!"
+  elsif guess > number 
+    msg = "Too high. Guess again!"
+  elsif  guess < number 
+    msg  = "Too low. Guess again" 
+  else 
+    msg = "Didn't hit one of the cases"
+  end
+  
+
+  erb :index, :locals => {:number => number, :msg => msg}
 end
 
