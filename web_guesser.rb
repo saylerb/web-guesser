0e92a35836a6fb475d7dcb45c9a722ff1782e02c
saylerb <sayler.b@gmail.com>
2016-05-03 18:16:51 -0600

    game is now playable

--- web_guesser.rb
+++ web_guesser.rb
@@ -3,22 +3,27 @@ require 'sinatra/reloader'
 
 number = rand(100)
 
-# aasdfasdfasdsaf
-
 get '/' do
   guess = params[:guess].to_i 
 
   # require "pry"; binding.pry
   if guess == number
-    msg = "You guessed the number!"
+    msg = "You guessed the number!\nThe secret number was #{number}"
   elsif guess > number 
-    msg = "Too high. Guess again!"
+    if guess - number > 5
+      msg = "Way too high! Guess again"
+    else
+      msg = "Too high. Guess again!"
+    end
   elsif  guess < number 
-    msg  = "Too low. Guess again" 
+    if number - guess > 5 
+      msg = "Way too low! Guess again"
+    else
+      msg  = "Too low. Guess again" 
+    end
   else 
     msg = "Didn't hit one of the cases"
   end
-  
 
   erb :index, :locals => {:number => number, :msg => msg}
 end
