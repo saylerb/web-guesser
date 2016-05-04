ca72afcbd990fc1d75e89151b8e47751a6f94c72
saylerb <sayler.b@gmail.com>
2016-05-03 20:43:08 -0600

    add guess limiter

--- web_guesser.rb
+++ web_guesser.rb
@@ -3,14 +3,28 @@ require 'sinatra/reloader'
 require_relative 'responses'
 
 set :secret, rand(100)
-enable :debug_mode
+disable :debug_mode
+@@total_guesses = 0
+previous_guess ||= nil
 
 get '/' do
   guess = params[:guess].to_i 
   msg, background_color = check_guess(guess, settings.secret)
-  debug_msg = generate_debug(guess, settings.secret)
+
+  check_total_guesses(previous_guess, guess)
+  previous_guess = guess
+
+  debug_msg = generate_debug(previous_guess, guess, settings.secret)
   erb :index, :locals => {:msg => msg, :debug_msg => debug_msg,
                           :background_color => background_color}   # :number => number
+end
+
+def check_total_guesses(previous_guess, guess)
+  @@total_guesses += 1 unless guess == previous_guess
+  if @@total_guesses == 5
+    settings.secret = rand(100)
+    @@total_guesses = 0
+  end
 end
 
 def check_guess(guess, number)
@@ -31,9 +45,13 @@ def check_guess(guess, number)
   end
 end
 
-def generate_debug(guess, number)
+def generate_debug(previous_guess, guess, number)
   if settings.debug_mode
     "DEBUG MODE:" + "<br>" + "The secret number is #{number}." +
-    "<br>" + "Previous guess is #{guess}." 
+    "<br>" + "Previous guess is #{previous_guess}." +
+    "<br>" + "Total guesses: #{@@total_guesses}."
+  else
+    "Your previous guess was #{previous_guess}." +
+    "<br>" + "Total guesses: #{@@total_guesses}."
   end
 end
