05459adea983d24c8c2a3bb98e9e458b004255d8
saylerb <sayler.b@gmail.com>
2016-05-03 19:27:32 -0600

    add previous guess to debug mode

--- web_guesser.rb
+++ web_guesser.rb
@@ -7,20 +7,25 @@ disable :debug_mode
 get '/' do
   guess = params[:guess].to_i 
   msg = check_guess(guess, settings.secret)
-  debug_msg = generate_debug(settings.secret)
+  debug_msg = generate_debug(guess, settings.secret)
   erb :index, :locals => {:msg => msg, :debug_msg => debug_msg}   # :number => number
 end
 
 def check_guess(guess, number)
   if guess > number 
-    guess - number > 5 ? "Way too high! Guess again" : "Too high. Guess again!"
+    guess - number > 5 ? "Way too high! Guess again." : "Too high. Guess again!"
   elsif guess < number 
-    number - guess > 5 ? "Way too low! Guess again" : "Too low. Guess again"
+    number - guess > 5 ? "Way too low! Guess again." : "Too low. Guess again"
   else
-    "You guessed the number!\nThe secret number was #{number}."
+    "You guessed the number!"+"<br>"+"The secret number was #{number}."
   end
 end
 
-def generate_debug(number)
-  settings.debug_mode ? "DEBUG MODE: The Secret Number is #{number}." : ""
+def generate_debug(guess, number)
+  if settings.debug_mode
+    "DEBUG MODE:" + "<br>" + "The secret number is #{number}." +
+    "<br>" + "Previous guess is #{guess}." 
+  else
+    ""
+  end
 end
